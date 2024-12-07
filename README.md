69158031981b527894eb61d8f9881225a1aa85cb
rotany <rotany38@gmail.com>
2024-12-07 19:13:11 +0100

    obteniendo imagenes

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # scraping-images-final_fantasy
+se tiene que hacer un python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+playwright install
--- main.py
+++ main.py
@@ -1,5 +1,33 @@
-from urllib.request import urlopen
+from playwright.sync_api import sync_playwright
 from bs4 import BeautifulSoup
-url = "http://www.hubertiming.com/results/2017GPTR10K"
-html = urlopen(url)
-print(html)
+
+
+url = "https://ffdecks.com/card/1"
+
+# Iniciar Playwright
+with sync_playwright() as p:
+    # Abrir un navegador Chromium
+    browser = p.chromium.launch(headless=True)  # headless=False para ver el navegador
+    page = browser.new_page()
+
+    # Navegar a la URL
+    page.goto(url)
+
+    # Esperar 5 segundos
+    page.wait_for_timeout(10000)
+
+    # Obtener el contenido de la página
+    html = page.content()
+
+    # Usar BeautifulSoup para analizar el HTML
+    soup = BeautifulSoup(html, 'lxml')
+
+    # Extraer imágenes
+    imagenes = soup.find_all('img')
+    print("Imágenes encontradas:")
+    for img in imagenes:
+        if 'https' in img['src']:
+            print(img['src'])
+
+    # Cerrar el navegador
+    browser.close()
