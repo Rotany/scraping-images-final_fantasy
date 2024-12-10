19857e5df82e71cb4edefd8fb811936a19e067aa
rotany <rotany38@gmail.com>
2024-12-10 18:51:21 +0100

    obteniendo imagenes

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from playwright.sync_api import sync_playwright
 from bs4 import BeautifulSoup
 
 
-url = "https://ffdecks.com/card/1"
+f = open("imagenes.txt", "w")
+
+
 
 # Iniciar Playwright
 with sync_playwright() as p:
@@ -10,24 +12,26 @@ with sync_playwright() as p:
     browser = p.chromium.launch(headless=True)  # headless=False para ver el navegador
     page = browser.new_page()
 
-    # Navegar a la URL
-    page.goto(url)
+    for numero in range(1,21):
+        url = f"https://ffdecks.com/card/{numero}" 
 
-    # Esperar 5 segundos
-    page.wait_for_timeout(10000)
+        page.goto(url)
 
-    # Obtener el contenido de la página
-    html = page.content()
+        # Esperar 5 segundos
+        page.wait_for_timeout(10000)
 
-    # Usar BeautifulSoup para analizar el HTML
-    soup = BeautifulSoup(html, 'lxml')
+        # Obtener el contenido de la página
+        html = page.content()
 
-    # Extraer imágenes
-    imagenes = soup.find_all('img')
-    print("Imágenes encontradas:")
-    for img in imagenes:
-        if 'https' in img['src']:
-            print(img['src'])
+        # Usar BeautifulSoup para analizar el HTML
+        soup = BeautifulSoup(html, 'lxml')
 
+        # Extraer imágenes
+        imagenes = soup.find_all('img')
+        for img in imagenes:
+            if 'https' in img['src']:
+                f.write(f"{img['src']} - Card {numero}\n")
+                
+    f.close()
     # Cerrar el navegador
     browser.close()
